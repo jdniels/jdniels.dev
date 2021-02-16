139a5717bf0ce270d675f06c0db81c8d7dd3c13a
Juan Daniel <jdniels100@gmail.com>
2021-02-16 19:38:15 +0700

    choir: reformat code

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
-import Analytics from "../components/analytics/analytics";
+import Analytics from '../components/analytics/analytics';
 
 export default function Home({ allPostsData }) {
   return (
