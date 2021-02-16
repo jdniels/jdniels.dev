18b639c5397f39c49c73bbd92e2ca771feb68eae
Juan Daniel <jdniels100@gmail.com>
2021-02-16 19:26:48 +0700

    choir: reformat code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
+import Analytics from '../components/analytics/analytics';
 
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
+        <Analytics />
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
@@ -35,14 +37,14 @@ export default function Home({ allPostsData }) {
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+    },
+  };
 }
